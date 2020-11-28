d693382a8448cb0bc0ffd72a59eb7e2b2dea3a0d
takafumi.kobayashi <takabumi.k@gmail.com>
2020-11-28 23:14:59 +0900

    async awati add3

--- src/linebots/service/instagram.service.ts
+++ src/linebots/service/instagram.service.ts
@@ -14,36 +14,40 @@ export class InstagramService {
 
     hashtagSearch(hashtag: string): string {
  
+        let hashtagId: string = null
         // 受け取ったキーワードでhashtag Search
-        return this.FB.api(
+        this.FB.api(
             '/ig_hashtag_search',
             'GET',
             {'access_token':process.env.INSTA_ACCESS_TOKEN,'user_id':process.env.INSTA_USER_ID,'q':'vertrek' + hashtag},
             function(response) {
                 if (response.data !== undefined) {
-                    return response.data[0].id
+                    hashtagId = response.data[0].id
                 }
             }
-        )   
+        )
+        console.log('hashitagId = ' + hashtagId)
+        return hashtagId
     }
 
     topMediaByHashtagId(hashtagId: string): Fbapi[] {
 
+        var resjson  = []
         // ハッシュタグIDのTOP-Media取得
-        return this.FB.api(
+        this.FB.api(
             '/' + hashtagId + '/top_media',
             'GET',
             {'access_token':process.env.INSTA_ACCESS_TOKEN,'fields':'like_count,media_url,permalink','limit':LinebotsConst.InstagramPrams.HASHTAG_SERCH_LIMIT,'user_id':process.env.INSTA_USER_ID},
             function(response) {
-                var resjson  = []
+                
                 response.data.forEach(data => {
                     resjson['like_count'] = data.like_count
                     resjson['media_url'] = data.media_url
                     resjson['permalink'] = data.permalink
                 })
-                return resjson
             }
         )
+        return resjson
     }
 
     topMediaByUserId(): Fbapi[] {
